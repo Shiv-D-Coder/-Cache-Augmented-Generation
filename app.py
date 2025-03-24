4000713295d6055fe9f394029ec871b1a72c4493
Shiv-D-Coder <shivtheworld@gmail.com>
2025-03-24 23:27:06 +0530

    Change in req.txt and app.py docx

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import asyncio
 import PyPDF2
 import docx
+from extract_docx import ExtractDocx
 from groq import AsyncGroq
 import json
 import time
@@ -17,12 +18,18 @@ def extract_text_from_pdf(pdf_path):
         reader = PyPDF2.PdfReader(file)
         return ''.join([page.extract_text() for page in reader.pages])
 
+# def extract_text_from_docx(docx_path):
+#     doc = docx.Document(docx_path)
+#     full_text = []
+#     for para in doc.paragraphs:
+#         full_text.append(para.text)
+#     return '\n'.join(full_text)
+
+from extract_docx import ExtractDocx
+
 def extract_text_from_docx(docx_path):
-    doc = docx.Document(docx_path)
-    full_text = []
-    for para in doc.paragraphs:
-        full_text.append(para.text)
-    return '\n'.join(full_text)
+    ed = ExtractDocx(docx_path)
+    return ed.extract_text()
 
 def load_knowledge_cache(file_path, max_length=6000):
     file_extension = file_path.split('.')[-1].lower()
